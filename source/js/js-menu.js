dbd95667ca95a001bcd2395cb913dfe00cd8c408
Julia Kalmykova <36233661+Kroshka-Shi@users.noreply.github.com>
2019-04-30 17:38:21 +0300

    optimize svg and rastr

--- source/js/js-menu.js
+++ source/js/js-menu.js
@@ -1,16 +1,30 @@
+"use strict";
+
 var toggle = document.querySelector(".top__toggle");
 var menu = document.querySelector(".main-nav__bottom");
 
-function hideOnMedia() {
-  if (menu.classList.contains("visually-hidden")) {
-    menu.classList.remove("visually-hidden");
-    toggle.classList.add("top__toggle--close");
-    menu.classList.toggle("open");
+var BREAKPOINTS = {
+  mobile: "320px",
+  tablet: "768px",
+  desktop: "1150px"
+};
 
+function hideOnMedia(e) {
+  if (e.matches) {
+    menu.classList.remove("visually-hidden");
   } else {
     menu.classList.add("visually-hidden");
     toggle.classList.remove("top__toggle--close");
   }
 }
 
-toggle.addEventListener("click", hideOnMedia);
+menu.classList.add("visually-hidden");
+
+var mediaEvent = window.matchMedia("(min-width: " + BREAKPOINTS.tablet.toString() + ")");
+hideOnMedia(mediaEvent);
+mediaEvent.addListener(hideOnMedia);
+
+toggle.addEventListener("click", function() {
+    menu.classList.toggle("visually-hidden");
+    toggle.classList.toggle("top__toggle--close");
+});
